e1bec594394dd5e82bc55d24b4a00ab24379883e
GorlevoyNickita <gorlevoynick2547@gmail.com>
2024-11-24 13:49:40 +0100

    Rotating and Scaling the Popup

--- Assets/SpaceWar/Script/AsteroidHit.cs
+++ Assets/SpaceWar/Script/AsteroidHit.cs
@@ -25,7 +25,12 @@ public class AsteroidHit : MonoBehaviour
 
         //Set our text for the popup -  then instiate the popup
         popupCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = asteroidScore.ToString();
-        Instantiate(popupCanvas, transform.position, Quaternion.identity);
+        GameObject asteroidPopup = Instantiate(popupCanvas, transform.position, Quaternion.identity);
+
+        //adjust the scale of the popup
+        asteroidPopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 10),
+                                                        transform.localScale.y * (distanceFromPlayer / 10),
+                                                        transform.localScale.z * (distanceFromPlayer / 10));
 
         //pass score to GameController 
         gameController.UpdatePlayerScore(asteroidScore);
